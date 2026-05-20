30b5c4019af4f4120e09a9d673640b9ac36ca567
ScreamAngel <emirkalender3453@gmail.com>
2026-05-20 18:11:04 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,22 +1,12 @@
 <?php
-// Render'ın vereceği ortam değişkenleri
-$host = getenv("DB_HOST");
-$user = getenv("DB_USER");
-$pass = getenv("DB_PASS");
-$dbname = getenv("DB_NAME");
-$port = "5432"; // PostgreSQL varsayılan portu
-
-try {
-    // PostgreSQL için DSN (Data Source Name) bağlantı dizesi
-    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
+    $servername = "localhost:3306";
+    $username = "root";
+    $password = "emir1234%";
+    $dbname = "pc";
     
-    // PDO nesnesini oluştur
-    $conn = new PDO($dsn, $user, $pass);
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
     
-    // Hataları daha net görebilmek için hata modunu aktif et
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-} catch (PDOException $e) {
-    die("Bağlantı hatası: " . $e->getMessage());
-}
+    if(!$conn){
+        die("Bağlantı Hatası: " . mysqli_connect_error());
+    }
 ?>
